625e64202ea2a665d0fa901b35cf25d67dfe6438
anshika25102004-ops <anshika25102004@gmail.com>
2026-06-22 18:41:10 +0530

    Update grade_calculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,1 +1,15 @@
+percentage = (450/500) * 100
 
+if percentage >= 90:
+    grade = "A+"
+elif percentage >= 80:
+    grade = "A"
+elif percentage >= 70:
+    grade = "B"
+elif percentage >= 60:
+    grade = "C"
+else:
+    grade = "F"
+
+print("Percentage:", percentage)
+print("Grade:", grade)
